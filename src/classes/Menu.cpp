3619a7492daf2c80d33982e7f0dfb6c539c84dd5
Patrick <patrick.barbosa@shareeat.com.br>
2021-06-07 07:32:30 -0300

    ✨ Init Menu Class

---
+++ src/classes/Menu.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../headers/Menu.h"
+
+using namespace std;
+
+Menu::Menu() {
+  cout << "Menu starting" << endl;
+}
+
+Menu::~Menu() {
+  cout << "Stop Menu" << endl;
+}
