ec81edb337c9c7bcaf35db6ff6469f588745461a
Patrick <patrick.barbosa@shareeat.com.br>
2021-06-13 23:08:41 -0300

    🏷️ Create Track struct

---
+++ src/headers/Track.h
@@ -0,0 +1,22 @@
+struct Track {
+  char id[22];
+  char name[255];
+  int popularity;
+  int duration_ms;
+  bool isExplicit;
+  char artists[1000];
+  char id_artists[440];
+  char release_date[10];
+  float danceability;
+  float energy;
+  int key;
+  float loudness;
+  bool mode;
+  float speechiness;
+  float acousticness;
+  float instrumentalness;
+  float liveness;
+  float valence;
+  float tempo;
+  int time_signature;
+};
