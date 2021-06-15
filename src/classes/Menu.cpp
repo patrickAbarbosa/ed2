876e40439bdfbb97bff8facb48557feed48e7489
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2021-06-15 07:25:55 -0300

    🚧 Menu in progress

--- src/classes/Menu.cpp
+++ src/classes/Menu.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <fstream>
 #include "../headers/Menu.h"
 #include "../headers/ReadFile.h"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -11,8 +11,21 @@ Menu::Menu() {
   loadTrackFile();
 }
 
-Menu::~Menu() {
-  cout << "Stop Menu" << endl;
+Menu::~Menu() { cout << "Stop Menu" << endl; }
+
+void Menu::loadTrackFile() {
+  string fileLocation;
+  ifstream binaryFile("../bin/tracks.bin", ifstream::in | ifstream::binary);
+
+  cout << "Digite o local do arquivo tracks: ";
+  cin >> fileLocation;
+
+  ReadFile *file;
+  file = new ReadFile(fileLocation);
+
+  TrackList *list = file->readBinaryTracks();
+
+  list->printList();
 }
 
 void Menu::loadTrackFile(){
