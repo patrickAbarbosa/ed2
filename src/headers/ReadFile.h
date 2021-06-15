9cdd5eb5fdad6e68c6e3dff4458e31ffd5188163
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2021-06-15 07:21:03 -0300

    🎨 Improves ReadFile

--- src/headers/ReadFile.h
+++ src/headers/ReadFile.h
@@ -18,6 +18,7 @@ class ReadFile {
     Track *readTrackLine(string line, ofstream *binaryFile);
     string readValue(stringstream *line);
     Track* readBinaryTrackLine(ifstream *binaryFile, int index);
+    int calculateBinaryTrackPosition(string variableName, int currentLine);
 };
 
 #endif
