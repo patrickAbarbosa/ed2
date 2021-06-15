9b25df91e5f7e5acaa575e86bc97697e00f921b1
Patrick Barbosa <pbarbosa@zak.app>
2021-06-14 22:07:07 -0300

    🚧 In progress ReadFile

--- src/classes/Menu.cpp
+++ src/classes/Menu.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include "../headers/Menu.h"
+#include "../headers/ReadFile.h"
 
 using namespace std;
 
 Menu::Menu() {
   cout << "Menu starting" << endl;
+  char menuItem;
+
+
+  loadTrackFile();
 }
 
 Menu::~Menu() {
   cout << "Stop Menu" << endl;
+}
+
+void Menu::loadTrackFile(){
+  string fileLocation;
+  cout << "Digite o local do arquivo tracks: ";
+  cin >> fileLocation;
+
+  ReadFile * file;
+  file = new ReadFile(fileLocation);
+
+  TrackList *list = file->readTracks();
+
+  list->printList();
 }
--- src/headers/Menu.h
+++ src/headers/Menu.h
@@ -7,6 +7,9 @@ class Menu {
 	public:
 		Menu();
 		~Menu();
+
+  private:
+    void loadTrackFile();
 };
 
 #endif
