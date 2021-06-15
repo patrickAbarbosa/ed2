8df9c5b487446a8a46dad3afcc7232ea4930bb25
Patrick Barbosa <pbarbosa@zak.app>
2021-06-14 22:01:56 -0300

    ✨ Create NodeTrack class

--- src/headers/NodeTrack.h
+++ src/headers/NodeTrack.h
@@ -9,7 +9,7 @@ class NodeTrack {
 		NodeTrack(Track *track) { this->track = track; nextTrack = NULL; };
 		~NodeTrack() { delete track; };
 
-    Track *getTrack() { return track; };
+    Track* getTrack() { return track; };
     NodeTrack *getNext() { return nextTrack; };
     void setNext(NodeTrack *nextTrack) { this->nextTrack = nextTrack; };
 
