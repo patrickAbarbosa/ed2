5c54378ca60e02101a4908038b86d1dcdcbdb103
Patrick Barbosa <patrickbarbosa@ice.ufjf.br>
2021-06-19 22:52:03 -0300

    ♻️ Refactor Menu and ReadFile

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
+/**
+ * main
+ *
+ * @param {int} argc Número de argumentos passados no terminal
+ * @param {char*[]} argv Argumentos passados po no terminal
+ */
 int main (int argc, char *argv[]) {
-  Menu menuInterface;
+  string trackFileLocation = "";
+  string artistFileLocation = "";
+
+  if (argc >= 2) {
+    trackFileLocation = argv[1];
+  }
+  if (argc >= 3) {
+    artistFileLocation = argv[2];
+  }
+
+  Menu menuInterface(trackFileLocation, artistFileLocation);
+  menuInterface.visible();
 
   return 0;
 }
--- src/classes/Menu.cpp
+++ src/classes/Menu.cpp
@@ -5,11 +5,96 @@
 
 using namespace std;
 
-Menu::Menu() {
+/**
+ * hasBinaryFile
+ *
+ * Verifica se existe arquivo binário no diretório <projectFolder>/bin
+ *
+ * @param {string} binaryFileName
+ *
+ * @return {bool}
+ */
+bool Menu::hasBinaryFile (string binaryFileName) {
+  string _binaryFileLocation = "../bin/" + binaryFileName;
+
+  ifstream binaryFile(_binaryFileLocation, ifstream::in | ifstream::binary);
+
+  if (binaryFile.is_open()) {
+    binaryFile.close();
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * Menu
+ *
+ * Inicia variáveis necessárias e verifica previamente
+ * se é necessário leitura de arquivos .csv
+ *
+ * @param {string} tracksFileLocation
+ * @param {string} artistsFileLocation
+ */
+Menu::Menu(string tracksFileLocation, string artistsFileLocation) {
+  readFile = new ReadFile(tracksFileLocation, artistsFileLocation);
+
+  char optionMenu;
+
+  if (hasBinaryFile("Tracks.bin")) {
+    cout << "Existe um arquivo binario de Tracks." << endl;
+    cout << "Deseja ler novamente um arquivo .csv? (s/n): ";
+    cin >> optionMenu;
+
+    bool isValidOption = optionMenu == 's' || optionMenu == 'S' || optionMenu == 'n' || optionMenu == 'N';
+
+    while (!isValidOption) {
+      cout << "Digite uma opcao valida!" << endl;
+      cout << "Deseja ler novamente um arquivo .csv? (s/n): ";
+      cin >> optionMenu;
+    }
+
+    if (optionMenu == 's' || optionMenu == 'S') {
+      readFile->readTracksFile();
+    }
+  } else if (tracksFileLocation.empty()) {
+    cout << "Arquivo Tracks invalido!" << endl;
+    exit(1);
+  }
+
+if (hasBinaryFile("Artists.bin")) {
+    cout << "Existe um arquivo binario de Artists." << endl;
+    cout << "Deseja ler novamente um arquivo .csv? (s/n): ";
+    cin >> optionMenu;
+
+    bool isValidOption = optionMenu == 's' || optionMenu == 'S' || optionMenu == 'n' || optionMenu == 'N';
+
+    while (!isValidOption) {
+      cout << "Digite uma opcao valida!" << endl;
+      cout << "Deseja ler novamente um arquivo .csv? (s/n): ";
+      cin >> optionMenu;
+    }
+
+    if (optionMenu == 's' || optionMenu == 'S') {
+      readFile->readArtistsFile();
+    }
+  } else if (artistsFileLocation.empty()) {
+    cout << "Arquivo Artists invalido!" << endl;
+    exit(1);
+  }
+}
+
+Menu::~Menu() {}
+
+/**
+ * visible
+ *
+ * Interface do usuário. Exibe opções opções possíveis.
+ */
+void Menu::visible() {
   int menuOption = -1;
-  ReadFile * file;
-  file = new ReadFile("");
-  // loadTrackFile();
+
   while (menuOption != 9) {
     cout << "---------- MENU -----------\n";
     cout << "[0] - Exibir estrutura Track\n";
@@ -24,45 +109,31 @@ Menu::Menu() {
     cout << endl;
     switch (menuOption)
     {
+    // [0] - Exibir estrutura Track
     case 0:
       cout << "Print estrutura Track" << endl;
+      readFile->printTrackStruct();
       break;
+    // [1] - Exibir estrutura Artists
     case 1:
       cout << "Print estrutura Artist" << endl;
       break;
+    // [2] - Testar Tracks
     case 2:
-      file->testBinaryTrackFile();
+      readFile->testBinaryTrackFile();
       break;
-
+    // [3] - Testar Artists
     case 3:
       cout << "Testar Artists" << endl;
       break;
+
+    // [9] - Sair
     case 9:
       cout << "Bye =)" << endl;
       break;
     default:
-      cout << "Erro: Digite uma opcao válida!" << endl;
+      cout << "Erro: Digite uma opcao valida!" << endl;
       break;
     }
   }
-}
-
-Menu::~Menu() { cout << "Encerrando Menu" << endl; }
-
-void Menu::loadTrackFile(){
-  string fileLocation;
-  // ifstream binaryFile("../bin/tracks.bin", ifstream::in | ifstream::binary);
-
-  cout << "Digite o local do arquivo tracks: ";
-  cin >> fileLocation;
-
-  ReadFile * file;
-  file = new ReadFile(fileLocation);
-
-  TrackList * list = file->readTracks();
-  // TrackList *list = file->readBinaryTracks();
-
-  // ArtistList *list = file->readBinaryArtists();
-
-  // list->printList();
 }
